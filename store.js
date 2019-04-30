bb664f4ce3dc910ff4c9c1a1a95670c4077a6b98
WilKhalifa <43878318+WilKhalifa@users.noreply.github.com>
2019-04-30 12:21:45 +0200

    Encrypt passwords

--- store.js
+++ store.js
@@ -15,3 +15,30 @@ module.exports = {
     })
   }
 }
+
+const crypto = require('crypto')
+module.exports = {
+  saltHashPassword,
+  createUser ({ username, password }) {
+    console.log(`Add user ${username}`)
+    const { salt, hash } = saltHashPassword(password)
+    return knex('user').insert({
+      salt,
+      encrypted_password: hash,
+      username
+    })
+  }
+}
+function saltHashPassword (password) {
+  const salt = randomString()
+  const hash = crypto
+    .createHmac('sha512', salt)
+    .update(password)
+  return {
+    salt,
+    hash: hash.digest('hex')
+  }
+}
+function randomString () {
+  return crypto.randomBytes(4).toString('hex')
+}
